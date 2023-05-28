6df2fe31a8f63808dfbf9e08d917ab01e3b15c9a
Raitin PC <jhosh1212@gmail.com>
2023-05-29 02:07:09 +0700

    delete copyOf and create for loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,8 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Main {
     public static void main(String[] args) {
         // Создание экземпляра ArrayList
